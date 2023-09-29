0c48f3acc41d8e3d8e7189f92ef79639e136d2f8
3nt3 <gott@3nt3.de>
2023-09-29 03:47:02 +0200

    honestly I have no idea what I changed.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,46 @@
-<main class="flex h-screen items-center justify-center bg-black p-32">
+<main class="flex h-screen items-center justify-center bg-zinc-950 p-32">
   <div class="flex h-full justify-center items-center gap-16 w-full">
     <div class="flex flex-col justify-start">
       <h1
-        class="font-display text-9xl -ml-1 bg-clip-text text-transparent bg-display-gradient"
+        class="font-display text-8xl -ml-1 bg-clip-text text-transparent bg-display-gradient"
       >
         Hi there,
       </h1>
-      <h2 class="font-serif text-3xl text-zinc-100 mt-1">
-        I&#8217;m Nia and I do <span class="italic">multiple</span> things.
+      <h2 class="font-serif text-3xl text-zinc-100 mt-1 z-10">
+        <!-- the escaped thing is a left quote -->
+        I&#8217;m Nia and I do
+        <span class="italic">
+          multiple
+          <!-- TODO: make this work -->
+          <!-- <svg
+            class="relative inline-block translate-x-[-75%] translate-y-[-10%] scale-[70%] "
+            width="221"
+            height="73"
+            viewBox="0 0 221 73"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            ><title />
+            <path
+              d="M101.993 27.1737C45.3577 23.7347 -1.04368 31.3341 3.27957 49.4614C7.60282 67.5887 108.838 69.9661 108.838 69.9661C108.838 69.9661 218 71.7491 218 49.4614C218 27.1737 89.0236 -5.81202 47.953 5.18323"
+              stroke="#C285FF"
+              stroke-opacity="0.82"
+              stroke-width="5"
+            />
+          </svg> -->
+        </span> things.
       </h2>
 
-      <button class="text-zinc-100 mt-8 mr-auto bg-purple-500 px-24 py-5 text-2xl rounded-full shadow-[0px_0px_30px_0px_rgba(161,103,234,0.60)]">
+      <button
+        class="text-zinc-100 mt-8 mr-auto bg-purple-500 px-24 py-5 text-2xl rounded-full shadow-[0px_0px_30px_0px_rgba(161,103,234,0.60)]"
+      >
         Find out more
       </button>
     </div>
     <img
-      class="object-scale-down  h-[120%]"
+      class="object-scale-down h-[120%]"
       src="/hero image.png"
       alt="Hero displaying a couple of projects."
     />
+    <div class="text-zinc-900" />
   </div>
 </main>
