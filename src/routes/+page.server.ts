b930a3a0616c2836be0e6980d405eff280696b37
3nt3 <gott@3nt3.de>
2025-07-29 10:38:34 +0200

    add hcaptcha
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer';
 import type { RequestEvent } from '@sveltejs/kit';
-import { MAIL_PASSWORD } from '$env/static/private';
+import { MAIL_PASSWORD, HCAPTCHA_SECRET } from '$env/static/private';
 
 const transporter = nodemailer.createTransport({
   host: "mail.3nt3.de",
@@ -10,17 +10,23 @@ const transporter = nodemailer.createTransport({
     user: "contact@3nt3.de",
     pass: MAIL_PASSWORD,
   },
-  tls : { rejectUnauthorized: false }
+  tls: { rejectUnauthorized: false }
 });
 
 
 export const actions = {
-  contact: async ({request}: RequestEvent) => {
+  contact: async (event: RequestEvent) => {
+    const { request } = event;
     const data = await request.formData();
 
     const name = data.get('name');
     const email = data.get('email');
     const message = data.get('message');
+    const hCaptchaResponse = data.get('h-captcha-response');
+
+    const clientAddress = event.getClientAddress();
+
+    if (!verifyHCaptcha(hCaptchaResponse?.toString(), clientAddress)) { return { status: 403, body: "captcha not passed" } }
 
     console.log(name, email, message);
 
@@ -39,3 +45,19 @@ export const actions = {
   }
 }
 
+
+async function verifyHCaptcha(hCaptchaResponse: string | undefined, clientAddress: string | undefined): Promise<boolean> {
+  const data = new URLSearchParams();
+  data.append('secret', HCAPTCHA_SECRET || '');
+  data.append('remoteip', clientAddress || '');
+  data.append('response', hCaptchaResponse || '');
+
+
+  const res = await fetch('https://api.hcaptcha.com/siteverify', { method: 'POST', body: data, headers: { 'Content-Type': 'application/x-www-form-urlencoded' } });
+  if (res.ok) {
+    const json = await res.json();
+    return json.success === true;
+  }
+
+  return false;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,12 @@
   import {
     faPaperPlane,
     faGraduationCap,
-    faWrench
+    faWrench,
   } from "@fortawesome/free-solid-svg-icons";
+
+  import HCaptcha from "svelte-hcaptcha";
+
+  let captchaCompleted = $state(false);
 </script>
 
 <nav
@@ -95,7 +99,9 @@
         alt="Portrait of myself"
         class="rounded-xl w-full h-96 xl:h-full object-cover overflow-hidden"
       />
-      <span class="absolute bottom-4 ml-4 z-10 font-serif text-xl">That's me!</span>
+      <span class="absolute bottom-4 ml-4 z-10 font-serif text-xl"
+        >That's me!</span
+      >
     </div>
     <section
       class="flex-1 flex flex-col gap-4 bg-zinc-900 p-8 rounded-xl hover:shadow-zinc-800 shadow-lg transition-shadow"
@@ -120,19 +126,16 @@
         <div class="w-[75%] font-bold bg-amber-500 px-4 py-2 rounded-lg">
           Backend ★
         </div>
-        <div class="w-[65%] font-bold bg-indigo-600 px-4 py-2 rounded-lg">
-          Data-Science/Web-Scraping
+        <div class="w-[60%] font-bold bg-indigo-600 px-4 py-2 rounded-lg">
+          Mechanical Engineering
         </div>
-        <div class="w-[43%] font-bold bg-indigo-600 px-4 py-2 rounded-lg">
-          Embedded
-        </div>
-        <div class="w-[42%] font-bold bg-indigo-600 px-4 py-2 rounded-lg">
-          Mechanical Eng.
+        <div class="w-[50%] font-bold bg-indigo-600 px-4 py-2 rounded-lg">
+          Embedded/Electronics
         </div>
       </div>
       <p class="font-serif">★ Featured Skill</p>
       <p class="font-serif text-sm text-zinc-500">
-        Note: unmarked x axis for added credibility.
+        Note: unmarked x-axis for added credibility.
       </p>
     </section>
   </div>
@@ -146,7 +149,7 @@
   <h2 class="italic font-serif text-xl mx-auto max-w-fit">
     What I've been up to.
   </h2>
-  <Carousel class="mt-10"/>
+  <Carousel class="mt-10" />
 </section>
 <section class="text-zinc-200 bg-zinc-950 lg:px-40 lg:py-12 px-8 z-10 py-12">
   <div class="flex justify-center gap-8 lg:flex-row flex-col">
@@ -155,12 +158,15 @@
         EDUCATION <Fa class="inline ml-4" icon={faGraduationCap} />
       </h3>
       <div class="p-2 text-lg">
-        <ul class="flex flex-col gap-2 mt-4 border-dashed border-l-2 border-purple-200">
+        <ul
+          class="flex flex-col gap-2 mt-4 border-dashed border-l-2 border-purple-200"
+        >
           {#each ["2015–2023 Abitur, Gymnasium Haan", "2023–2024 B. Sc., Computer Engineering, Cologne University of Applied Sciences (dropped out)", "2024–now B. Eng., Mechanical Engineering, Bochum University of Applied Sciences"] as item}
             <li class="flex gap-4">
-              <div class="mt-1 -ml-2 w-4 h-4 rounded-full bg-purple-200"> </div>
+              <div class="mt-1 -ml-2 w-4 h-4 rounded-full bg-purple-200"></div>
               <div class="flex-1">
-                <span class="font-bold">{item.split(",")[0]},</span> {item.split(",").slice(1).join(",")}
+                <span class="font-bold">{item.split(",")[0]},</span>
+                {item.split(",").slice(1).join(",")}
               </div>
             </li>
           {/each}
@@ -172,16 +178,15 @@
         WORK <Fa class="inline ml-4" icon={faWrench} />
       </h3>
       <div class="p-2 text-lg">
-        <ul class="flex flex-col gap-2 mt-4 border-dashed border-l-2 border-purple-200">
-          {#each ["2019–2023, Newspaper Delivery Job (2 hours/week)",
-                  "2023–2024, Working Student Full-Stack Development (React, PHP), Biz Factory GmbH (20 hours/week)",
-                  "2024, Working Student DevOps (Linux, PHP, Docker), Check24 Ferienwohnungen GmbH (16 hours/week)",
-                  "2024–now, Dual Student Mechanical Engineering, Rheinbahn AG (39 hours/week)"
-                  ] as item}
+        <ul
+          class="flex flex-col gap-2 mt-4 border-dashed border-l-2 border-purple-200"
+        >
+          {#each ["2019–2023, Newspaper Delivery Job (2 hours/week)", "2023–2024, Working Student Full-Stack Development (React, PHP), Biz Factory GmbH (20 hours/week)", "2024, Working Student DevOps (Linux, PHP, Docker), Check24 Ferienwohnungen GmbH (16 hours/week)", "2024–now, Dual Student Mechanical Engineering, Rheinbahn AG (39 hours/week)"] as item}
             <li class="flex gap-4">
-              <div class="mt-1 -ml-2 w-4 h-4 rounded-full bg-purple-200"> </div>
+              <div class="mt-1 -ml-2 w-4 h-4 rounded-full bg-purple-200"></div>
               <div class="flex-1">
-                <span class="font-bold">{item.split(",")[0]},</span> {item.split(",").slice(1).join(",")}
+                <span class="font-bold">{item.split(",")[0]},</span>
+                {item.split(",").slice(1).join(",")}
               </div>
             </li>
           {/each}
@@ -190,19 +195,23 @@
     </div>
   </div>
 </section>
-<section class="text-zinc-200 bg-zinc-950 lg:p-40 lg:pt-12 px-8 z-10 pt-12" id="contact">
+<section
+  class="text-zinc-200 bg-zinc-950 lg:p-40 lg:pt-12 px-8 z-10 pt-12"
+  id="contact"
+>
   <div class="bg-zinc-900 p-12 rounded-xl gap-16 flex">
     <div class="w-2/6 min-h-[300px] hidden lg:block text-clip">
       <h1
         class="font-display text-zinc-500 text-5xl break-all [word-spacing: -20px] text-block select-none aria-hidden"
       >
-        LEAVE A MESSAGE AT THE TONE —
-        LEAVE A MESSAGE AT THE TONE —
-        LEAVE A MESSAGE AT THE TONE
+        LEAVE A MESSAGE AT THE TONE — LEAVE A MESSAGE AT THE TONE — LEAVE A
+        MESSAGE AT THE TONE
       </h1>
     </div>
     <div class="flex flex-col w-full items-center">
-      <h1 class="font-display text-5xl bg-display-gradient-lighter bg-clip-text text-transparent">
+      <h1
+        class="font-display text-5xl bg-display-gradient-lighter bg-clip-text text-transparent"
+      >
         CONTACT
       </h1>
       <h2 class="italic font-serif text-xl text-zinc-200">
@@ -231,11 +240,39 @@
           class="bg-zinc-800 border-zinc-700 border-2 text-zinc-200 p-4 rounded-lg mt-4 h-48"
           required
         ></textarea>
-        <button
-          class="bg-purple-500 text-zinc-200 p-4 mt-4 font-bold rounded-full hover:shadow-[0px_0px_10px_0px_rgba(161,103,234,0.60)] transition-shadow ring-zinc-200 active:ring-3"
-        >
-          SEND IT <Fa class="inline" icon={faPaperPlane} />
-        </button>
+        <div class="flex flex-col md:flex-row md:justify-evenly mt-4 md:items-center justify-center gap-4">
+          <div class="flex-1 flex justify-center">
+            <HCaptcha
+              sitekey={"e190725a-e624-4dc0-a10c-622a0524443e"}
+              theme="dark"
+              on:success={(event) => {
+                console.log("Captcha success:", event.detail);
+                captchaCompleted = true;
+              }}
+              on:error={(event) => {
+                console.error("Captcha error:", event.detail);
+                captchaCompleted = false;
+              }}
+            />
+          </div>
+          <!-- Tooltip wrapper -->
+          <div class="relative flex-1 group shrink" style="min-width: 120px;">
+            <button
+              disabled={!captchaCompleted}
+              class="bg-purple-500 text-zinc-200 p-4 font-bold rounded-full hover:shadow-[0px_0px_10px_0px_rgba(161,103,234,0.60)] transition-shadow ring-zinc-200 active:ring-3 w-full disabled:opacity-50 pointer"
+            >
+              SEND IT <Fa class="inline ml-2" icon={faPaperPlane} />
+            </button>
+
+            {#if !captchaCompleted}
+              <div
+                class="absolute bottom-full mb-2 left-1/2 transform -translate-x-1/2 px-3 py-1 rounded bg-purple-500 text-white text-xs whitespace-nowrap pointer-events-none opacity-0 group-hover:opacity-100 transition-opacity"
+              >
+                Please complete the captcha first
+              </div>
+            {/if}
+          </div>
+        </div>
       </form>
     </div>
   </div>
