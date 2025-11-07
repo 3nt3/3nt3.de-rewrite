f5bfa02467b4a489de7d61e3aed5bab794b156ed
Nia Schlegel <gott@3nt3.de>
2025-11-07 17:05:15 +0100

    improve all styling

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,16 +3,16 @@
 </script>
 
 <nav
-  class="sticky top-0 w-screen flex bg-zinc-900 h-20 z-30 lg:px-40 px-8 items-center text-lg text-zinc-300"
+  class="sticky top-0 w-full flex bg-zinc-900 h-20 z-30 px-12 items-center text-lg text-zinc-300 justify-center"
 >
-  <ul class="flex justify-between w-full">
+  <ul class="flex justify-between w-full max-w-[1600px]">
     <li class="font-display text-purple-400 text-xl font-bold">
       <a href="/">Nia Schlegel</a>
     </li>
-    <ul class="flex gap-8 font-bold">
+    <ul class="gap-8 font-bold flex">
       <li class=""><a href="/blog">Blog</a></li>
-      <li class=""><a href="/#about">About</a></li>
-      <li class=""><a href="/#contact">Contact</a></li>
+      <li class="md:block hidden"><a href="/#about">About</a></li>
+      <li class="md:block hidden"><a href="/#contact">Contact</a></li>
     </ul>
   </ul>
 </nav>
