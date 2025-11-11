a66679503e9131d3a29c43ec6e3643ce0225937d
3nt3 <gott@3nt3.de>
2025-11-11 17:21:52 +0100

    remove build
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- build/server/chunks/1-C8jydL2D.js
+++
@@ -1,9 +0,0 @@
-const index = 1;
-let component_cache;
-const component = async () => component_cache ??= (await import('./error.svelte-6ywKxA_F.js')).default;
-const imports = ["_app/immutable/nodes/1.23a17c6f.js","_app/immutable/chunks/scheduler.fc6b6886.js","_app/immutable/chunks/index.83c0f759.js","_app/immutable/chunks/singletons.48f1dd9e.js","_app/immutable/chunks/index.31ac934a.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=1-C8jydL2D.js.map
--- build/server/chunks/_layout.svelte-BTi1Ew5R.js
+++
@@ -1,8 +0,0 @@
-import { c as create_ssr_component } from './ssr-DS8CioHm.js';
-
-const Layout = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `${slots.default ? slots.default({}) : ``}`;
-});
-
-export { Layout as default };
-//# sourceMappingURL=_layout.svelte-BTi1Ew5R.js.map
